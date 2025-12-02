d497856342602ccc2d4239c79ea6e69bc803ff18
miguel <miguelangelminambres@gmail.com>
2025-12-02 15:19:45 +0100

    Corregir vercel.json

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-6fc00345'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.0ngi6oqbr2o"
+    "revision": "0.6ab9ed7ur7s"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
